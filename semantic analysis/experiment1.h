c45bf285a9644ce362da38105578aee6de4234fa
coder-pc <43802228+BertrandGauss@users.noreply.github.com>
2021-06-25 12:17:20 +0800

    Create experiment1.h

---
+++ semantic analysis/experiment1.h
@@ -0,0 +1,26 @@
+#ifndef _EXPERIMENT1_H  
+#define _EXPERIMENT1_H  
+#include <string>
+#include <map> 
+using namespace std;
+#define GRAMMAR_BASE 40 //动态生成的基值  
+
+struct node{//记号流节点 
+	string word;
+	string type;
+	int number; 
+	int row;
+	int col;
+	node* next;
+};
+
+
+void initKeyword();
+void initDelimiter();
+void initOperate();
+void init();
+node* lex();
+int* getRowCol();
+
+
+#endif 
